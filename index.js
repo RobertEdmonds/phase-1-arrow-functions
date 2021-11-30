570123e41189c879fc3535240d3faee67827f8e1
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-29 17:52:57 -0700

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = 2000;
+let num2 = 100;
+const divide = function (){
+    return num1 / num2;
+}
+divide();
+
+const square = number => number * number;
+square()
+
+const add = (a, b) => {return a + b}
+add();
